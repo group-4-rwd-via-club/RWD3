19bbbf953229569106232d20cc46758390ecec3f
CHANHaochengZhang <47462485+CHANHaochengZhang@users.noreply.github.com>
2019-05-04 14:43:30 +0200

    dblclickfish implement

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 /*globals $:false, window:false, document:false */
 
 // Start animations upon load
-$(document).ready(function(){
-    animateRandom('#fish1Id', false);
-    animateRandom('#fish2Id', false);
-    onHover('#fish2Id');
-    animateBubbles('#bubble1Id');
-    animateBubbles('#bubble2Id');
-    animateBubbles('#bubble3Id');
+$(document).ready(function () {
+	animateRandom('#fish1Id', false);
+	animateRandom('#fish2Id', false);
+	onHover('#fish2Id');
+	animateBubbles('#bubble1Id');
+	animateBubbles('#bubble2Id');
+	animateBubbles('#bubble3Id');
+	clickfish('#fish1Id');
+	dblclickfish('#fish1Id');
 });
 
 
@@ -23,40 +25,45 @@ $(document).ready(function(){
 /// at a random entry point in the bottom of the screen.
 
 var bubbleAnimationTime = 10000;
-function animateBubbles(itemId){
-    
-    
-    
-    var sw = getRandomStartPosBottom(itemId);
-    var sh = $(window).height();
-    $(itemId).offset({top: sh, left: sw})
-    $(itemId).animate({ top: -100}, bubbleAnimationTime,   function(){
-        //rerun the animation
-        animateBubbles(itemId);        
-    }).click(function(){
-        // stop animations and start fadeout
-        $(itemId).stop();
-        $(itemId).fadeOut(400, function(){
-            // make sure every animation is stopped before contineuing 
-            // this removes a ms delay in the animation
-            $(itemId).stop(true);
-            // make sure it is faded in again before start
-            $(itemId).fadeIn(1);
-            animateBubbles(itemId);
-        });
 
-        
-    });
+function animateBubbles(itemId) {
+
+
+
+	var sw = getRandomStartPosBottom(itemId);
+	var sh = $(window).height();
+	$(itemId).offset({
+		top: sh,
+		left: sw
+	})
+	$(itemId).animate({
+		top: -100
+	}, bubbleAnimationTime, function () {
+		//rerun the animation
+		animateBubbles(itemId);
+	}).click(function () {
+		// stop animations and start fadeout
+		$(itemId).stop();
+		$(itemId).fadeOut(400, function () {
+			// make sure every animation is stopped before contineuing 
+			// this removes a ms delay in the animation
+			$(itemId).stop(true);
+			// make sure it is faded in again before start
+			$(itemId).fadeIn(1);
+			animateBubbles(itemId);
+		});
+
+
+	});
 }
 
 
 
-function getRandomStartPosBottom(itemId)
-{
-    var offsetW = $(itemId).innerWidth();
-    var w = $(window).width() - offsetW;
-    var nw = Math.floor(Math.random() * w);
-    return nw;
+function getRandomStartPosBottom(itemId) {
+	var offsetW = $(itemId).innerWidth();
+	var w = $(window).width() - offsetW;
+	var nw = Math.floor(Math.random() * w);
+	return nw;
 }
 
 
@@ -69,44 +76,77 @@ function getRandomStartPosBottom(itemId)
 
 // Returns random position within window screen minus
 // items height and width.
-function moveToRandomPosition(dx, dy){
-    
-    // dx dy makes sure items move within screen window
-    var h = $(window).height() - dy;
-    var w = $(window).width() - dx;
-    
-    var nh = Math.floor(Math.random() * h);
-    var nw = Math.floor(Math.random() * w);
-    
-    return [nh,nw];    
-    
+function moveToRandomPosition(dx, dy) {
+
+	// dx dy makes sure items move within screen window
+	var h = $(window).height() - dy;
+	var w = $(window).width() - dx;
+
+	var nh = Math.floor(Math.random() * h);
+	var nw = Math.floor(Math.random() * w);
+
+	return [nh, nw];
+
 }
 // variable to set movespeed of the fish
 var fishMovementTime = 10000;
 // Randomly moves the item
-function animateRandom(itemId, fast){
+function animateRandom(itemId, fast) {
 
-    var fishSpeed = fishMovementTime;
-    if (fast){
-        fishSpeed = 500;
-    }
-    // get items height and width.
-    var x = $(itemId).innerWidth();
-    var y = $(itemId).innerHeight();
-    // get random pos as x,y array.
-    var newPos = moveToRandomPosition(x,y);
-    $(itemId).animate({ top: newPos[0], left: newPos[1] }, fishSpeed,   function(){
-        //rerun the animation
-        animateRandom(itemId, false);        
-    });
-    
+	var fishSpeed = fishMovementTime;
+	if (fast) {
+		fishSpeed = 500;
+	}
+	// get items height and width.
+	var x = $(itemId).innerWidth();
+	var y = $(itemId).innerHeight();
+	// get random pos as x,y array.
+	var newPos = moveToRandomPosition(x, y);
+	$(itemId).animate({
+		top: newPos[0],
+		left: newPos[1]
+	}, fishSpeed, function () {
+		//rerun the animation
+		animateRandom(itemId, false);
+	});
+
 }
 
-function onHover(itemId){
-    $(itemId).mouseover(function(){
-        $(itemId).stop(true);
-        animateRandom(itemId, true);
+function onHover(itemId) {
+	$(itemId).mouseover(function () {
+		$(itemId).stop(true);
+		animateRandom(itemId, true);
 
-        
-    })
+
+	})
+}
+
+//orange fish
+function clickfish(itemId) {
+	var img = $(itemId);
+	var iw = img.width();
+	var ih = img.height();
+
+	$(img).click(function (event) {
+		img.css({
+			left: Math.floor(event.pageX - iw / 2),
+			top: Math.floor(event.pageY - ih / 2)
+		});
+	});
+}
+
+function dblclickfish(itemId) {
+	var img = $(itemId);
+
+	$(img).dblclick(function () {
+		img.css({
+			transform: "scale(2)"
+		});
+		setTimeout(function () {
+			img.css({
+				transform: "scale(1)"
+			});
+
+		}, 1500);
+	})
 }
